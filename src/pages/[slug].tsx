f76471be561b4a21d887e9e8121d3ac084e8125e
pontakornth <pontakorn61@gmail.com>
2021-10-30 19:37:48 +0700

    feat: Include mdx

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { H1 } from "../components/elements/Heading";
+
+const BlogPostPage: NextPage = () => {
+	return (
+		<H1>Post Code</H1>
+	)
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from 'next/app'
+import { MDXProvider } from '@mdx-js/react'
 import globalStyles from '../styles/globalStyles'
 
 const App = ({ Component, pageProps }: AppProps) => {
   globalStyles()
-  return <Component {...pageProps} />
+  return (
+    <MDXProvider>
+      <Component {...pageProps} />
+    </MDXProvider>
+  )
 }
 
 export default App
