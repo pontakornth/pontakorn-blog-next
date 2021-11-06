40f95ce5912181773f3510abf839f3782b24d173
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2021-11-06 14:22:00 +0700

    feat: Include navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { H1, H2 } from '../components/elements/Heading'
 import Head from 'next/head'
 import { BlogPost } from '../core/@types/BlogPost'
 import type { GetStaticProps, InferGetStaticPropsType } from 'next'
+import { Navbar } from '../components/modules/Navbar'
 
 const Container = tw.div`text-center`
 
@@ -11,20 +12,23 @@ const Container = tw.div`text-center`
 const IndexPage = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { total, items } = posts as { total: number, items: BlogPost[] }
   return (
-    <Container>
-      <Head>
-        <title>Pontakorn Blog</title>
-      </Head>
-      <H1 color="primary">Pontakorn Blog</H1>
-      {items.map(item => (
-        <>
-          <Link passHref href={`/${item.slug}`}>
-            <H2 as="a">{item.title}</H2>
-          </Link>
-          <p tw="mb-4">{item.description}</p>
-        </>
-      ))}
-    </Container>
+    <>
+      <Navbar></Navbar>
+      <Container>
+        <Head>
+          <title>Pontakorn Blog</title>
+        </Head>
+        <H1 color="primary">Pontakorn Blog</H1>
+        {items.map(item => (
+          <>
+            <Link passHref href={`/${item.slug}`}>
+              <H2 as="a">{item.title}</H2>
+            </Link>
+            <p tw="mb-4">{item.description}</p>
+          </>
+        ))}
+      </Container>
+    </>
   )
 }
 
