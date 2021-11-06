f1114e103b2ca8d61fbf034171cf46e1dff1bbd2
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2021-11-06 14:21:19 +0700

    feat: Add font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
       colors: {
         electric: '#db00ff',
         ribbon: '#0047ff',
+        primary: '#67AAF9',
       },
+      fontFamily: {
+        sans: "'IBM Plex Sans Thai Looped', sans-serif",
+        display: "'IBM Plex Sans Thai', sans-serif"
+      }
     },
   },
   plugins: [],
