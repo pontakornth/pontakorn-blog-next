961dc2ba1f70ddd01dff6e976caece42c9c2d3c0
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2021-11-06 14:22:07 +0700

    feat: Include font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,7 +29,11 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@400;700&family=IBM+Plex+Sans+Thai:wght@400;700&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
