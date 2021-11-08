ebfae8954b5110005540d247958b9506f59eec6f
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2021-11-08 10:02:51 +0700

    fix: Cross origin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,7 +31,7 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@400;700&family=IBM+Plex+Sans+Thai:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
