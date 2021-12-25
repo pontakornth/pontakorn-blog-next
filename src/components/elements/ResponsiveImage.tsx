e6405957c1946bdd9408429a535c4daf292e0900
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2021-12-25 16:33:07 +0700

    chore: Remove unused files

--- src/components/elements/ResponsiveImage.tsx
+++
@@ -1,5 +0,0 @@
-import Image from "next/image"
-import { ImageProps } from "next/image"
-export const ResponsiveImage = (props: ImageProps) => {
-	return <Image alt={props.alt} layout="fill" {...props} />
-}
--- src/components/modules/PostCard.tsx
+++ src/components/modules/PostCard.tsx
@@ -1,21 +1,25 @@
-import tw from 'twin.macro'
-import { H2 } from '../elements/Heading'
-import Link from 'next/link'
+import tw from "twin.macro";
+import { H2 } from "../elements/Heading";
+import Link from "next/link";
 export interface PostCardProps {
-	title: string,
-	description: string,
-	link: string
+  title: string;
+  description: string;
+  link: string;
 }
-export const PostCard: React.FC<PostCardProps> = ({ title, description, link }) => {
-	return (
-		<div tw="p-4 rounded border border-primary shadow-lg">
-			<H2 tw="text-primary underline">
-				<Link href={link} passHref>
-					<a>{title}</a>
-				</Link>
-			</H2>
-			<p>{description}</p>
-		</div>
-	)
+export const PostCard: React.FC<PostCardProps> = ({
+  title,
+  description,
+  link,
+}) => {
+  return (
+    <div tw="p-4 rounded border border-primary shadow-lg">
+      <H2 tw="text-primary underline">
+        <Link href={link} passHref>
+          <a>{title}</a>
+        </Link>
+      </H2>
+      <p>{description}</p>
+    </div>
+  );
+};
 
-}
