6b2ebfbe322e7f8864bfef8de9542be215127634
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2021-12-25 21:51:46 +0700

    fix: Invalid link

--- src/components/modules/PostCard.tsx
+++ src/components/modules/PostCard.tsx
@@ -9,8 +9,8 @@ export const PostCard: React.FC<PostCardProps> = ({ post }) => {
   return (
     <div tw="p-4 rounded border border-primary shadow-lg text-left">
       <H2 tw="text-primary underline text-3xl">
-        <Link href={`https:://blog.pontakorn.dev/${post.slug}`} passHref>
-          <a>{post.title}</a>
+        <Link href={`https://blog.pontakorn.dev/${post.slug}`} passHref>
+          <a target="__blank">{post.title}</a>
         </Link>
       </H2>
       <p>{post.brief}</p>
